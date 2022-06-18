fd6b885088bcd679f6065a940ced4dda9d5ad9d6
MohamedSaidCS <60986229+MohamedSaidCS@users.noreply.github.com>
2022-06-18 21:02:50 +0200

    Todo Controller CRUD placeholder methods

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -6,5 +6,28 @@ use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'index';
+    }
+
+    public function show($id)
+    {
+        return 'show';
+    }
+
+    public function store(Request $request)
+    {
+        return 'store';
+    }
+
+    public function update(Request $request, $id)
+    {
+        return 'update';
+    }
+
+    public function destroy($id)
+    {
+        return 'destroy';
+    }
 }
